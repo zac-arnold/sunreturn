773f69116fe2ea419604c6cf0ec839972a1e2863
Zac Arnold <videozac@gmail.com>
2020-09-28 11:33:23 +1300

    add logo and contents to nav component

---
+++ src/Nav.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='/SRLogo.jpg' className="App-logo" alt="logo" />
+        <h1>
+          SUNRETURN
+        </h1>
+        <p>
+        Sunreturn is an independent record label based in Tāmaki Makaurau, Aotearoa. We strive to amplify the work of artists who find freedom in imagining new and unique sounds.
+        </p>
+        <h2>
+          AMAMELIA  
+        </h2>      
+        <h2>
+          BABY ZIONOV 
+        </h2>      
+        <h2>
+          DATELINE 
+        </h2>      
+        <h2>
+          GREEN GROVE 
+        </h2>      
+        <h2>
+          K M T P 
+        </h2>
+        <img src='/Spotify.png' className="App-logo" alt="logo" />
+        <img src='/Facebook.svg' className="App-logo" alt="logo" />
+        <img src='/Instagram.png' className="App-logo" alt="logo" />
+        <img src='/Email Logo.svg' className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
