c2a35e3c3f2d0a419073f787f7103de308712505
Zac Arnold <videozac@gmail.com>
2020-09-29 21:31:20 +1300

    end of night commit

--- src/Artist.js
+++ src/Artist.js
@@ -1,38 +1,42 @@
-import React from 'react';
-import { Link, Route } from 'react-router-dom'
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom'
 
 import lineup from './lineupdata.json'
 
 import './Artist.css';
 
+console.log('artist.js lineup', lineup)
 
-function Artist() {
+class Artist extends Component() {
+  
+render () {
+ const { id } = this.props.match.params;
+  
+    return (
+      <div className="">
+        <header className="">
+          <img src="" alt=""></img>
+          <h2>
+          Artist Name { id }
+          </h2>
+          <p>
+          Bio
+          </p>
+          <p>
+          Photo Credit
+          </p>
+          <div className="artistLogo">
+          <img src='/Bandcamp.png' className="artistsoclogo" alt="logo" />
+          <img src='/Spotify.png' className="artistsoclogo" alt="logo" />
+          <img src='/Youtube.png' className="artistsoclogo" alt="logo" />
+          <img src='/Instagram.png' className="artistsoclogo" alt="logo" />
+          <img src='/Facebook.svg' className="artistsoclogo" alt="logo" />
+          </div>
+        </header>
+      </div>
+    );
+  }
 
-  console.log('artist.js lineup', lineup)
-
-  return (
-    <div className="">
-      <header className="">
-        <img src="" alt=""></img>
-        <h2>
-         Artist Name
-        </h2>
-        <p>
-         Bio
-        </p>
-        <p>
-         Photo Credit
-        </p>
-        <div className="artistLogo">
-        <img src='/Bandcamp.png' className="artistsoclogo" alt="logo" />
-        <img src='/Spotify.png' className="artistsoclogo" alt="logo" />
-        <img src='/Youtube.png' className="artistsoclogo" alt="logo" />
-        <img src='/Instagram.png' className="artistsoclogo" alt="logo" />
-        <img src='/Facebook.svg' className="artistsoclogo" alt="logo" />
-        </div>
-      </header>
-    </div>
-  );
 }
 
-export default Artist;
+export default withRouter (Artist)
