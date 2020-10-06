ab60dd3b9ff5560f2d227285ccadfa3c7e5eef8a
Zac Arnold <videozac@gmail.com>
2020-10-06 14:32:16 +1300

    trail more email embed and add discography

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,49 @@
 import React from 'react';
 
+import './Home.css';
+
 function Home() {
   return (
 
-    <div className="App">
-      <header className="App-header">
+    <div className="">
         <h1>
-        SUNRETURN HOME PAGE
+          DISCOGRAPHY
         </h1>
+
+        <div className="covers">
+          <img src="./MachineMusic.jpg" alt=""></img>
+          <p className="catnum">SR001</p>
+          <p>Green Grove - Machine Music</p>
+        </div>
+
+        <div className="covers">
+          <img src="./DatelineLive.jpg" alt=""></img>
+          <p className="catnum">SR002</p>
+          <p>Dateline - Dateline Live!</p>
+        </div>
+      
+        <div className="covers">
+          <img src="./BBZ.jpg" alt=""></img>
+          <p className="catnum">SR003</p>
+          <p>Baby Zionov - ....And You Are The Bitch</p>
+        </div>
+
+        <div className="covers">
+          <img src="./KMTPPSCUSOON.jpg" alt=""></img>
+          <p className="catnum">SR004</p>
+          <p>K M T P - P.S. C U SOON</p>
+        </div>
+
+        <div className="covers">
+          <img src="./AmameliaWow.jpg" alt=""></img>
+          <p className="catnum">SR005</p>
+          <p>Amamelia - WOW!</p>
+        </div>
+
         <p>
           THIS IS WHERE I WANT THE EMAIL EMBED TO GO 
         </p> 
-      </header>
+
     </div>
   );
 }
